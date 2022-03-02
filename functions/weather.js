c8e881e88cd9ca98715c65af9acf175da82d52c1
Marcus <marcuscey@gmail.com>
2022-03-02 10:52:14 +0000

    Resolved bug in weather.js to accurate reading

--- functions/weather.js
+++ functions/weather.js
@@ -1,28 +1,32 @@
 async function weatherFunc(weatherLayer){
     let responseWeather = await getWeather()
+    // console.log(responseWeather)
     for (weather of responseWeather.area_metadata){
+      // console.log(weather)
       let weatherMarker = null;
       let weatherCoordinate = [
         weather.label_location.latitude,
         weather.label_location.longitude,
       ];
+      
       for(weatherType of responseWeather.items[0].forecasts){
-        if(weatherType.forecasts === "Cloudy" || "Partly Cloudy (Day)" || "Partly Cloudy (Night)"){
+        console.log(weatherType.forecast)
+        if(weatherType.forecast == "Cloudy" || weatherType.forecast =="Partly Cloudy (Day)" || weatherType.forecast == "Partly Cloudy (Night)"){
           weatherMarker = L.marker(weatherCoordinate, {icon:weatherIconCloudy});
         }
-        else if(weatherType.forecasts === "Showers" || "Moderate Rain" || "Light Rain" || "Light Showers" || "Heavy Showers"){
+        else if(weatherType.forecast == "Showers" || weatherType.forecast == "Moderate Rain" || weatherType.forecast == "Light Rain" || weatherType.forecast == "Light Showers" || "Heavy Showers"){
           weatherMarker = L.marker(weatherCoordinate, {icon:weatherIconShower});
         }
-        else if(weatherType.forecasts === "Fair" || "Fair (Day)" || "Fair (Night)" ||"Light & Warm"){
+        else if(weatherType.forecast == weatherType.forecast == "Fair" || weatherType.forecast == "Fair (Day)" || weatherType.forecast == "Fair (Night)" ||weatherType.forecast == "Light & Warm"){
           weatherMarker = L.marker(weatherCoordinate, {icon:weatherIconFair});
         }
-        else if(weatherType.forecasts === "Thundery Showers" || "Heavy Showers" || "Heavy Thundery Showers" || "Heavy Thundery Showers with Gusty Winds"){
+        else if(weatherType.forecast == weatherType.forecast == "Thundery Showers" || weatherType.forecast == "Heavy Showers" || weatherType.forecast == "Heavy Thundery Showers" || weatherType.forecast == "Heavy Thundery Showers with Gusty Winds"){
           weatherMarker = L.marker(weatherCoordinate, {icon:weatherIconThunder});
         }
-        else if(weatherType.forecasts === "Hazy" || "Slightly Hazy" || "Misty"){
+        else if(weatherType.forecast == weatherType.forecast == "Hazy" || weatherType.forecast == "Slightly Hazy" || weatherType.forecast == "Misty"){
           weatherMarker = L.marker(weatherCoordinate, {icon:weatherIconHazy});
         }
-        else if(weatherType.forecasts === "Windy" ){
+        else if(weatherType.forecast == "Windy" ){
           weatherMarker = L.marker(weatherCoordinate, {icon:weatherIconWind});
         }
         weatherMarker.bindPopup(`<div>${weather.name}</div>`);
